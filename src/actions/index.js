827b6c51af4628887bc983c0432dca1f2848b176
Rodgers Chayuga <rchayuga@dvtsoftware.co.ke>
2021-12-09 19:49:54 +0300

    Add Redux action

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,17 @@ export const petDecrement = () => {
     type: "PET_DECREASED",
   };
 };
+
+export const petFavoriteIncrement = (number) => {
+  return {
+    type: "PET_FAVORITE_INCREASED",
+    payload: number,
+  };
+};
+
+export const petFavoriteDecrement = (number) => {
+  return {
+    type: "PET_FAVORITE_DECREASED",
+    payload: number,
+  };
+};
