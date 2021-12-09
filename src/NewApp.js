1e451044b68dc1a11440327287e4c9db4f98257b
Rodgers Chayuga <rchayuga@dvtsoftware.co.ke>
2021-12-09 19:51:24 +0300

    Add Pet to Favorite

--- src/NewApp.js
+++ src/NewApp.js
@@ -1,17 +1,24 @@
 import "./App.css";
-import { petIncrement, petDecrement } from "./actions";
+import {
+  petIncrement,
+  petDecrement,
+  petFavoriteIncrement,
+  petFavoriteDecrement,
+} from "./actions";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  const petCounter = useSelector((state) => state.petCounter);
   const petFavorite = useSelector((state) => state.petFavorite);
   const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>New component - Add Pet to Favorite</h1>
-      <button onClick={() => dispatch(petIncrement(2))}>Add pet</button>
-      <button onClick={() => dispatch(petDecrement())}>Remove pet</button>
-      <h1>Pet counter {petCounter}</h1>
+      <button onClick={() => dispatch(petFavoriteIncrement(20))}>
+        Add pet
+      </button>
+      <button onClick={() => dispatch(petFavoriteDecrement(10))}>
+        Remove pet
+      </button>
       <h2>Number of favorite pets so far {petFavorite}</h2>
     </div>
   );
