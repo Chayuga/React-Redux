3f79953ce6cf50b687fc401e20f1de2f95b810ae
Rodgers Chayuga <rchayuga@dvtsoftware.co.ke>
2021-12-09 19:50:38 +0300

    Add Redux Pet Favorite reducer

--- src/reducers/petFavorite.js
+++ src/reducers/petFavorite.js
@@ -1,9 +1,9 @@
 const petFavorite = (state = 5, action) => {
   switch (action.type) {
     case "PET_FAVORITE_INCREASED":
-      return state + 1;
+      return state + action.payload;
     case "PET_FAVORITE_DECREASED":
-      return state - 1;
+      return state - action.payload;
     default:
       return state;
   }
